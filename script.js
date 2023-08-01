78fc102715d227bd47f7936422d04420a3817bd5
Kevin <kevin.nguyen2321@yahoo.com>
2023-07-31 21:42:01 -0500

    Add working functions to UI without images or styling

--- script.js
+++ script.js
@@ -11,6 +11,35 @@ function getComputerChoice(params) {
     }
 }
 
+function resetScores () {
+        if (playerScore === 5 || computerScore === 5) {
+            if (playerScore === 5) {
+                liveScore.textContent = 'You won!';
+                
+            } else if (computerScore === 5) {
+                liveScore.textContent = 'You lost!'; 
+            }
+            
+            playerScore = 0;
+            myScore.textContent = 'My score:' + 0;
+            computerScore = 0;
+            compScore.textContent = 'Computer Score:' + 0;
+            newGame();        
+        }
+} 
+
+
+function newGame () {
+        buttons.forEach(button => button.classList.add('hide'));
+        playAgainBtn.classList.add('show');
+        playAgainBtn.addEventListener('click', (e)=> {
+            liveScore.textContent = "";
+            playAgainBtn.classList.remove('show');
+            e.currentTarget.classList.add('hide');
+            buttons.forEach(button => button.classList.remove('hide'));
+        });
+        
+    }
 
 
 
@@ -19,23 +48,20 @@ function getComputerChoice(params) {
 function playRound(playerSelection, computerSelection) {
      console.log(playerSelection);
      console.log(computerSelection);
-   
-    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+
+    
+     if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore++
         myScore.textContent = 'My score:' + `${playerScore}`;
         compScore.textContent = 'Computer Score:' + `${computerScore}`;
-        
         
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         computerScore++
         myScore.textContent = 'My score:' + `${playerScore}`;
         compScore.textContent = 'Computer Score:' + `${computerScore}`;
-        
-        
     
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors' ) {
-        computerScore++
-        playerScore++
+        
         myScore.textContent = 'My score:' + `${playerScore}`;
         compScore.textContent = 'Computer Score:' + `${computerScore}`;
         
@@ -53,8 +79,7 @@ function playRound(playerSelection, computerSelection) {
         
     
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        computerScore++
-        playerScore++
+        
         myScore.textContent = 'My score:' + `${playerScore}`;
         compScore.textContent = 'Computer Score:' + `${computerScore}`;
         
@@ -72,12 +97,12 @@ function playRound(playerSelection, computerSelection) {
 
     
     } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        computerScore++
-        playerScore++
         compScore.textContent = 'Computer Score:' + `${computerScore}`;
         myScore.textContent = 'My score:' + `${playerScore}`;
-    }  
-    }
+    }  resetScores();
+}
+     
+    
 
 
 let playerScore = 0;
@@ -88,30 +113,35 @@ let computerScore = 0;
 
 
 // Events for buttons on UI
-
+let buttons = document.querySelectorAll('.game-btns');
 let paperBtn = document.getElementById ('btn-paper');
 let rockBtn = document.getElementById('btn-rock');
 let scissorsBtn = document.getElementById ('btn-scissors');
 let myScore = document.getElementById ('my-score');
 let compScore = document.getElementById('comp-score');
+let playAgainBtn = document.getElementById ('play-again');
+let liveScore = document.getElementById('update');
+
 
 
 
 
 paperBtn.addEventListener('click', (e) => { 
     console.log(playRound('paper', getComputerChoice()));
-    
-    });
+
+}); 
+   
+
 
 rockBtn.addEventListener('click', (e) => { 
-        console.log(playRound('paper', getComputerChoice()));
+        console.log(playRound('rock', getComputerChoice()));
         
         }); 
         
 
 
 scissorsBtn.addEventListener('click', (e) => { 
-            console.log(playRound('paper', getComputerChoice()));
+            console.log(playRound('scissors', getComputerChoice()));
             
             });
 
